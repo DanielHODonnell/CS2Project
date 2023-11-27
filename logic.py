0b6a95c5af577255f2f7e8c8195b55d96620afeb
DanielHODonnell <danielodonnell@unomaha.edu>
2023-11-27 14:39:53 -0600

    Second

--- logic.py
+++ logic.py
@@ -3,17 +3,62 @@ from gui import *
 import sys
 
 
-
 class Logic(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.submit_button.clicked.connect(lambda: self.submit())
-        self.add_button.clicked.connect(lambda: self.add())
-        self.reset_button.clicked.connect(lambda: self.reset())
-        self.exit_button.clicked.connect(lambda: self.quit())
 
-    def quit(self):
-        sys.exit()
+        self.submit_button.clicked.connect(self.calculate_tax)
+        self.exit_button.clicked.connect(self.close)
+        self.reset_button.clicked.connect(self.clear_input)
 
+    def calculate_tax(self):
+        input_text = self.input_num.text()
+        if input_text:
+            try:
+                num = float(input_text)
+            except ValueError:
+                QtWidgets.QMessageBox.warning(
+                    None,
+                    "Warning",
+                    "Please enter a valid number"
+                )
+                return
+        else:
+            QtWidgets.QMessageBox.warning(
+                None,
+                "Warning",
+                "Please enter a number"
+            )
 
+        selected_city = self.comboBox_city.currentText()
+        tax_rates = {
+            "N/A": 0.055,
+            "Omaha": 0.07,
+            "Lincoln": 0.0725,
+            "Bellevue": 0.07,
+            "Grand Island": 0.075,
+            "Kearney": 0.07,
+            "Fremont": 0.07,
+            "Norfolk": 0.07,
+            "Hastings": 0.07,
+            "Columbus": 0.07,
+            "Papillion": 0.075
+        }
+        if selected_city in tax_rates:
+            tax_rate = tax_rates[selected_city]
+            sales_tax = num * tax_rate
+            QtWidgets.QMessageBox.information(
+                None,
+                "Sales Tax",
+                f"Sales Tax in {selected_city}: ${sales_tax:.2f}"
+            )
+        else:
+            QtWidgets.QMessageBox.warning(
+                None,
+                "Warning",
+                "Please select a valid city"
+            )
+
+    def clear_input(self):
+        self.input_num.clear()
